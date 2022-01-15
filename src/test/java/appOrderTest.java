7ae09f00b78a6829b4cb6705847fd5839d93d7d3
Валентина Юрова <visible7@mail.ru>
2022-01-15 17:37:29 +0500

    доработка

--- src/test/java/appOrderTest.java
+++ src/test/java/appOrderTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -5,84 +6,95 @@ import java.time.Duration;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
-public class appOrderTest {
-    private String nameFieldSelector = "#root > div > form > div:nth-child(1) > span > span > span.input__box > input";
-    private String phoneFieldSelector = "#root > div > form > div:nth-child(2) > span > span > span.input__box > input";
-    private String checkBoxSelector = "#root > div > form > div:nth-child(3) > label > span.checkbox__box";
-    private String buttonSelector = "#root > div > form > div:nth-child(4) > button";
+public class AppOrderTest {
+    private String nameFieldSelector = "[data-test-id='name'] input";
+    private String phoneFieldSelector = "[data-test-id='phone'] input";
+    private String checkBoxSelector = "[data-test-id='agreement'] .checkbox__box";
+    private String buttonSelector = "button[type='button']";
 
     @Test
     public void shouldSuccessfulSendFormWithCorrectData() {
-        open("http://localhost:9999");
-
         $(nameFieldSelector).setValue("Иванов Иван-Ивановский");
         $(phoneFieldSelector).setValue("+79228569875");
         $(checkBoxSelector).click();
         $(buttonSelector).click();
-        $("#root > div > div > p")
+        $("[data-test-id='order-success']")
                 .shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."), Duration.ofMillis(5000));
     }
 
     @Test
     public void shouldNotSendFormWithEmptyName() {
-        open("http://localhost:9999");
 
+        $(phoneFieldSelector).setValue("+79228569875");
+        $(checkBoxSelector).click();
         $(buttonSelector).click();
-        $("#root > div > form > div:nth-child(1) > span")
-                .shouldHave(cssClass("input_invalid"), Duration.ofMillis(2000));
+
+        $("[data-test-id='name'].input_invalid .input__sub")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     public void shouldNotSendFormWithIncorrectName() {
-        open("http://localhost:9999");
-
         $(nameFieldSelector).setValue("ggg ggg");
+        $(phoneFieldSelector).setValue("+79228569875");
+        $(checkBoxSelector).click();
         $(buttonSelector).click();
-        $("#root > div > form > div:nth-child(1) > span")
-                .shouldHave(cssClass("input_invalid"), Duration.ofMillis(2000));
+
+        $("[data-test-id='name'].input_invalid .input__sub")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     public void shouldNotSendFormWithEmptyPhone() {
-        open("http://localhost:9999");
-
         $(nameFieldSelector).setValue("Иванов Иван-Ивановский");
+        $(checkBoxSelector).click();
         $(buttonSelector).click();
-        $("#root > div > form > div:nth-child(2) > span")
-                .shouldHave(cssClass("input_invalid"), Duration.ofMillis(2000));
+
+        $("[data-test-id='phone'].input_invalid .input__sub")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     public void shouldNotSendFormWithIncorrectPhone() {
-        open("http://localhost:9999");
-
         $(nameFieldSelector).setValue("Иванов Иван-Ивановский");
         $(phoneFieldSelector).setValue("79228569875");
+        $(checkBoxSelector).click();
         $(buttonSelector).click();
-        $("#root > div > form > div:nth-child(2) > span")
-                .shouldHave(cssClass("input_invalid"), Duration.ofMillis(2000));
+
+        $("[data-test-id='phone'].input_invalid .input__sub")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     public void shouldNotSendFormWithIncorrectPhone2() {
-        open("http://localhost:9999");
-
         $(nameFieldSelector).setValue("Иванов Иван-Ивановский");
         $(phoneFieldSelector).setValue("+79228565");
+        $(checkBoxSelector).click();
         $(buttonSelector).click();
-        $("#root > div > form > div:nth-child(2) > span")
-                .shouldHave(cssClass("input_invalid"), Duration.ofMillis(2000));
+
+        $("[data-test-id='phone'].input_invalid .input__sub")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     public void shouldNotSendFormWithUncheckedCheckbox() {
-        open("http://localhost:9999");
-
         $(nameFieldSelector).setValue("Иванов Иван-Ивановский");
         $(phoneFieldSelector).setValue("+79228569875");
         $(buttonSelector).click();
 
-        $("#root > div > form > div:nth-child(3) > label")
-                .shouldHave(cssClass("input_invalid"), Duration.ofMillis(2000));
+        $("[data-test-id='agreement']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(cssClass("input_invalid"));
+    }
+
+    @BeforeEach
+    public void openBrowser() {
+        open("http://localhost:9999");
     }
 }
+
